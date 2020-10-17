67f385a4d8e1e7078128703a94149e1aaaefb91c
Olayiwola Olanrewaju <larry_coal@outlook.com>
2020-10-17 13:23:56 +0100

    should work

---
+++ build/precache-manifest.483d0d4662b2dfe4ce393decffbdf41e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f2a22cd83cd15b05659d995f8a6f63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae16338cc4001a45d51f",
+    "url": "/static/css/main.cfb0c982.chunk.css"
+  },
+  {
+    "revision": "f3c80dd7a1e0904d4628",
+    "url": "/static/js/2.076e58a4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.076e58a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae16338cc4001a45d51f",
+    "url": "/static/js/main.febaee2c.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
